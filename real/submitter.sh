e75813fa35d3426f250ff25013e3f3c9162dced0
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-22 15:27:55 +0100

    Added SLURM submitter for PBMC4K simulations.

---
+++ real/submitter.sh
@@ -0,0 +1,20 @@
+#################################################################
+# This script submits all simulation jobs to the SLURM scheduler.
+# The use of separate files makes it trivial to set up multiple jobs on different cores.
+# This allows timely execution of all of the scripts, yet with easily altered parameters.
+#################################################################
+
+mkdir -p logs
+R=/Users/lun01/software/R/devel/bin/R
+
+# Submitting the PBMC 4K simulations.
+
+sbatch << EOT
+#!/bin/bash
+#SBATCH -o logs/out-pbmc4k
+#SBATCH -e logs/err-pbmc4k
+#SBATCH -n 1
+#SBATCH --mem 16000
+
+echo 'source("run_pbmc4k.R")' | ${R} --slave --vanilla
+EOT
