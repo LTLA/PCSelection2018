16310dae237d1b49d4690326317eea106a47b8e0
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-21 10:49:32 +0100

    Added code to choose PCs in original data.

--- real/run_kolod.R
+++ real/run_kolod.R
@@ -4,3 +4,7 @@ source("../simulations/functions.R")
 full <- readRDS("Processed/kolod.rds")
 dir.create("results", showWarnings=FALSE)
 simulateReal(full$exprs, full$noise, prefix="results/kolod")
+
+# Seeing the values in practice.
+dec.out <- decomposer(full$exprs, nrank=50)
+chooseNumber(full$exprs, dec.out$SVD, full$noise)
--- real/run_pbmc4k.R
+++ real/run_pbmc4k.R
@@ -4,3 +4,7 @@ source("../simulations/functions.R")
 full <- readRDS("Processed/pbmc4k.rds")
 dir.create("results", showWarnings=FALSE)
 simulateReal(full$exprs, prefix="results/pbmc4k")
+
+# Seeing the values chosen in practice.
+dec.out <- decomposer(as.matrix(full$exprs), nrank=50, approximate=TRUE)
+chooseNumber(full$exprs, dec.out$SVD, full$noise)
